9731d6dca167d7d1cedf425f32a7e9ebd6917c3f
Kelsey <kelseymtheriault@gmail.com>
2018-05-16 16:00:31 -0700

    Webpack configuration

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, PropTypes} from 'react';
+import Example from '../containers/Example';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>React is running!</div>
+  }
+
+};
+
+export default App;
---
+++ app/containers/Example.js
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react';
+import { connect } from 'react-redux';
+import Example from '../components/Example';
+
+const mapStateToProps = () => {
+  return {}
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {};
+};
+
+const Example = connect(mapStateToProps, mapDispatchToProps)(Link);
+export default Example;
