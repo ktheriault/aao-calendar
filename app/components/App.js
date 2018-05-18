9a545ae2341174e5a568b45e0fc15c7dac5440d4
Kelsey <kelseymtheriault@gmail.com>
2018-05-17 17:51:13 -0700

    Only loads one event

--- app/components/App.js
+++ app/components/App.js
@@ -1,59 +1,19 @@
 import React, { Component } from "react";
 import EventSchedule from "../components/EventSchedule";
-import * as api from "../api";
+import { EVENT_ID } from "../global";
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            eventList: null,
-            selectedEventID: null,
-            isLoading: true,
-        };
-        this.onEventSelectedHandler = this.onEventSelectedHandler.bind(this);
-    }
-
-    async componentDidMount() {
-        let response = await api.getEventList();
-        this.setState({
-            eventList: response.events,
-            isLoading: false,
-        })
-    }
-
-    onEventSelectedHandler(eventID) {
-        return () => {
-            this.setState({
-                selectedEventID: eventID,
-            });
-        }
     }
 
     render() {
-        let { eventList, selectedEventID, isLoading } = this.state;
-        return isLoading ? (
-            <div style={{padding: "2em"}}>Loading event list...</div>
-        ) : (
+        return (
             <div style={{padding: "2em"}}>
-                <div>
-                    {eventList.map((event) => {
-                        return (
-                            <div key={event.id}>
-                                <button onClick={this.onEventSelectedHandler(event.id)}>
-                                    {event.title}
-                                </button>
-                            </div>
-                        )
-                    })}
-                </div>
-                {selectedEventID ? (
-                    <EventSchedule
-                        eventID={selectedEventID}
-                    />
-                ) : (
-                    <div>Select an event to see data!</div>
-                )}
+                <EventSchedule
+                    eventID={EVENT_ID}
+                />
             </div>
         );
     }
--- app/global.js
+++ app/global.js
@@ -1,3 +1,5 @@
+export const EVENT_ID = "213800ffda91408c9266c28b954e95f6";
+
 export const SCHEDULE_VIEWS = {
     FOR_DOCTORS: {
         key: "forDoctors",
