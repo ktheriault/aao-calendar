1ca63646a66a41a70f2c19908d70fa8354a7b381
Kelsey <kelseymtheriault@gmail.com>
2018-05-25 15:21:12 -0700

    Fix for building from server.js

--- app/store.js
+++ app/store.js
@@ -2,6 +2,8 @@ import { createStore } from "redux";
 
 import reducer from "./reducers/reducer";
 
+let window = window ? window : global.window;
+
 let store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
