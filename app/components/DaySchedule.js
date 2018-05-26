541e8470fff659a44ffda5901f439782b59ef484
Kelsey <kelseymtheriault@gmail.com>
2018-05-26 00:05:27 -0700

    No ScrollbarSize

--- app/components/DaySchedule.js
+++ app/components/DaySchedule.js
@@ -169,10 +169,12 @@ export default class DaySchedule extends React.Component {
                                 </div>
                             );
                         })}
-                        <ScrollbarSize
-                            onLoad={this.onScrollbarLoad}
-                            onChange={this.onScrollbarChange}
-                        />
+                        {/*
+                            <ScrollbarSize
+                                onLoad={this.onScrollbarLoad}
+                                onChange={this.onScrollbarChange}
+                            />
+                        */}
                     </div>
                 </div>
                 <SessionModal
